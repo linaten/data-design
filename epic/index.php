8874e871a351ce12bf0b2309efb586e50676d34f
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-08 13:44:35 -0600

    testing commit

---
+++ epic/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE=HTML>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="style.css"/>
+	<meta charset="UTF-8">
+	<title>Data Design</title>
+</head>
+<body>
+<a href="phase1.php">Phase One</a>
+</body>
+</html>
