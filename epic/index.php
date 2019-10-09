5dbddd3c7a2f24e43344dd0b71fa72f3e9b0c338
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-09 14:35:20 -0600

    Submission two

--- epic/index.php
+++ epic/index.php
@@ -6,6 +6,9 @@
 	<title>Data Design</title>
 </head>
 <body>
-<a href="phase1.php">Phase One</a>
+<a href="phase1.php">Persona</a>
+<a href="phase1a.php">User Story</a>
+<a href="phase1b.php">Use Case</a>
+<a href="phase1c.php">Conceptual Model</a>
 </body>
 </html>
